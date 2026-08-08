5f90ef72e4dee8c401e7f6696c59c6a3f5345269
lenadro291 <lenadrito1@gmail.com>
2026-08-08 14:01:40 -0500

    Replace TokenObtainPairView with UserLoginView for custom JWT login

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from users.api import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
     path('auth/register/', views.UserRegisterView.as_view(), name='user_register'),
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login/', views.UserLoginView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
