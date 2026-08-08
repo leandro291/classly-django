2d18b8910dd325edc1ec3b91499956ce173f1794
lenadro291 <lenadrito1@gmail.com>
2026-08-07 23:25:21 -0500

    Add custom User model with roles and additional fields

---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+
+    class Roles(models.TextChoices):
+        TEACHER = 'teacher', 'teacher'
+        STUDENT = 'student', 'student'
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=11, blank=True)
+
+    rol = models.CharField(
+        max_length=10,
+        choices=Roles.choices,
+        default=Roles.STUDENT
+    )
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ['-date_joined']
+
+    def __str__(self):
+        return self.email
