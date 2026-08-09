729db52fbeaeeb4d9328055cff1676dd76d7b162
lenadro291 <lenadrito1@gmail.com>
2026-08-08 21:51:52 -0500

    Remove unused User queryset from UserRegisterView

--- users/api/views.py
+++ users/api/views.py
@@ -2,10 +2,8 @@ from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.api.serializers import UserRegisterSerializer, UserLoginSerializer
-from users.models import User
 
 class UserRegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
 
 class UserLoginView(TokenObtainPairView):
