ae15bcb079f5a8b5e2d604cb0b1c9835140dcf0c
lenadro291 <lenadrito1@gmail.com>
2026-08-09 14:45:52 -0500

    Add serializers for Material and ArchivoMaterial with file handling

---
+++ contenido/api/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from contenido.models import Material, ArchivoMaterial
+from drf_spectacular.utils import extend_schema_field
+
+@extend_schema_field({
+    "type": "array",
+    "items": {
+        "type": "string",
+        "format": "binary"
+    }
+})
+class MultipleImageField(serializers.ListField):
+    child = serializers.FileField(allow_empty_file=False, use_url=False)
+
+class MaterialArchivoSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ArchivoMaterial
+        fields = ['id', 'file']
+
+    def get_file(self, obj):
+        return obj.file.url if obj.file else None
+
+class MaterialSerializer(serializers.ModelSerializer):
+    archivos = MultipleImageField(write_only=True, required=False)
+    archivo_materials = MaterialArchivoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Material
+        fields = ['id', 'course', 'title', 'description', 'created_at', 'archivos', 'archivo_materials']
+        read_only_fields = ['id', 'course', 'created_at']
+
+    def create(self, validated_data):
+        archivos = validated_data.pop('archivos', [])
+        material = Material.objects.create(**validated_data)
+
+        for archivo in archivos:
+            ArchivoMaterial.objects.create(
+                material=material,
+                file=archivo
+            )
+
+        return material
