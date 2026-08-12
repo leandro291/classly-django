7bd59f01e5dd95598ac7c6baa9b22b8cc7a1eb2e
lenadro291 <lenadrito1@gmail.com>
2026-08-11 20:47:15 -0500

    Add token refresh endpoint to authentication URLs

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from users.api import views
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('auth/register/', views.UserRegisterView.as_view(), name='user_register'),
     path('auth/login/', views.UserLoginView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', views.UserRefreshView.as_view(), name='token_refresh'),
 ]
